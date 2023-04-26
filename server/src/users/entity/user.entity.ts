caf041173e15a1f2e0547b6e5a6ba46adcc23a03
tanmayVaish <tanmay777leon@gmail.com>
2023-04-26 20:00:12 +0530

    user entity created

---
+++ server/src/users/entity/user.entity.ts
@@ -0,0 +1,25 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  areaCode: string;
+
+  @Column()
+  mobile: string;
+
+  @Column()
+  password: string;
+}
